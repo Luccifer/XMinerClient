483eefc953bccb8fa721b856b9a08cea20d6ede0
Gleb Karpushkin <gleb.karp@gmail.com>
2017-11-08 17:53:48 +0300

    Client init
    Monero mining!

---
+++ XMiner/Xminer.framework/Headers/HashContext.h
@@ -0,0 +1,16 @@
+//
+//  HashContext.h
+//  XMRMiner
+//
+//  Created by Nick Lee on 10/12/17.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface HashContext : NSObject
+
+- (NSData * _Nonnull)hashData:(NSData * _Nonnull)data;
+
+@end
---
+++ XMiner/Xminer.framework/Headers/Xminer.h
@@ -0,0 +1,25 @@
+//
+//  Xminer.h
+//  Xminer
+//
+//  Created by Gleb Karpushkin on 05/11/2017.
+//  Copyright © 2017 Gleb Karpushkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NSData+FastHex.h"
+#import "GCDAsyncSocket.h"
+#import "GCDAsyncUdpSocket.h"
+
+#import "HashContext.h"
+
+//! Project version number for Xminer.
+FOUNDATION_EXPORT double XminerVersionNumber;
+
+//! Project version string for Xminer.
+FOUNDATION_EXPORT const unsigned char XminerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Xminer/PublicHeader.h>
+
+
