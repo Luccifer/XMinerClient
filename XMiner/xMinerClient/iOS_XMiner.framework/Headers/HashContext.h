a6723c5ba393e2af03e6d1c5ccd600950768760f
Gleb Karpushkin <Luccifer@users.noreply.github.com>
2020-11-14 21:33:35 +0300

    Added precompiled framework

---
+++ XMiner/xMinerClient/iOS_XMiner.framework/Headers/HashContext.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+@interface HashContext : NSObject
+
+- (NSData * _Nonnull)hashData:(NSData * _Nonnull)data;
+
+@end
---
+++ XMiner/xMinerClient/iOS_XMiner.framework/Headers/iOS_XMiner.h
@@ -0,0 +1,26 @@
+//
+//  iOS_XMiner.h
+//  iOS_XMiner
+//
+//  Created by Gleb Karpushkin on 19/11/2017.
+//  Copyright © 2017 Gleb Karpushkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NSData+FastHex.h"
+
+#import "GCDAsyncUdpSocket.h"
+#import "GCDAsyncSocket.h"
+
+#import "HashContext.h"
+
+//! Project version number for iOS_XMiner.
+FOUNDATION_EXPORT double iOS_XMinerVersionNumber;
+
+//! Project version string for iOS_XMiner.
+FOUNDATION_EXPORT const unsigned char iOS_XMinerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOS_XMiner/PublicHeader.h>
+
+
